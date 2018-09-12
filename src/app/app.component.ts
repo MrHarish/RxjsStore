f0b8423237eac1dab55b4b76cc7aef1ea3bb066c
harishperiketi <ph.periketi@gmail.com>
2018-09-12 11:29:51 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, AfterContentInit, ViewChild, ViewContainerRef, ComponentFactoryResolver } from "@angular/core";
+import { Response} from "@angular/http";
+
+import { ChildComponent } from "./child/child.component";
+import { HttpService } from "./http.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterContentInit {
   title = 'app';
+  data ;  
+  @ViewChild('cont', {read: ViewContainerRef}) cont: ViewContainerRef;
+
+
+  
+
+  constructor(private resolver: ComponentFactoryResolver, private service: HttpService){ debugger}
+
+  ngAfterContentInit(){
+    const componentFactory = this.resolver.resolveComponentFactory(ChildComponent);
+    const component1 = this.cont.createComponent(componentFactory);
+  }
+
+  getdata(){
+    debugger
+    this.service.getInfo().subscribe((response: Response)=>{
+      this.data = response["holidays"];
+      console.log(this.data)
+
+    })
+  }
 }
